de7df6d602db9dca98bb3fc0b035cf66dc86c759
kykdev <kykint@naver.com>
2018-06-29 14:35:59 +0900

    Misc
    Signed-off-by: kykdev <kykint@naver.com>

--- dts.sh
+++ dts.sh
@@ -6,8 +6,8 @@ for BOOTIMG in *.img; do
   [ -d "${BOOTIMG}.out" ] && echo "${BOOTIMG}.out already exists, skipping" && continue
   $RDIR/extract-dtb.py -o $RDIR/"${BOOTIMG}.out" $RDIR/"$BOOTIMG"
   
-  for DTB in "${BOOTIMG}.out"/*dtbdump*; do
-    $RDIR/dtc -I dtb -O dts -o "$(echo $DTB | sed -e "s/\.dtb/\.dts/")" "$DTB"
+  for DTB in "${BOOTIMG}.out"/*.dtb; do
+    $RDIR/dtc -I dtb -O dts -o "${DTB%.*}.dts" "$DTB"
   done
 done
 
